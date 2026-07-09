c05819213d50e2762f1af90b4b68f3b2dc1b2cac
LUIS HENRIQUE MORGENSTERN PEREIRA <03683498021@senacrs.edu.br>
2026-07-09 17:23:12 -0300

    ave

---
+++ 7decisoes/4operadoresLogicos/script.js
@@ -0,0 +1,16 @@
+var variavel1 = 45;
+var variavel2 = true;
+var variavelTexto = "zero";
+var variavelNumero = "20";
+var variavelLogica = false;
+var variavelCurta = "texto curto";
+
+if(variavel2 && variavelNumero == 24) {
+    console.log("Retorno verdadeiro");
+} else {
+    console.log("Retorno Falso.");
+}
+if (variavel1 > 50) {console.log(variavel1 + "é a maior que 50"); }
+if (variavelTexto != "ue") {console.log(variavelTexto + "é diferente de um");}
+if (variavelLogica) {console.log("negação da variavel logica e verdadeira");}
+if (variavelTexto != variavel1) {console.log("valor das variaveis é diferente");}
