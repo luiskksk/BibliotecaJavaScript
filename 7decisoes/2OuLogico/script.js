1bcbc746aea0a4726cbe4551ba22680a4390e3cc
LUIS HENRIQUE MORGENSTERN PEREIRA <03683498021@senacrs.edu.br>
2026-07-09 16:57:46 -0300

    ave

---
+++ 7decisoes/2OuLogico/script.js
@@ -0,0 +1,10 @@
+var numeroCarros = 10;
+var todosCarrosMesmaMarca = false;
+
+if (numeroCarros === 5 || todosCarrosMesmaMarca === true) {
+    console.log("ou todos os carros são da mesma marca.");
+    console.log("ou temos cinco carros.");
+    console.log("ou as duas coisas juntos.");
+} else{
+    console.log("Não tenho cinco carros e nem todos são da mesma marca");
+}
---
+++ 7decisoes/3IfElse/script.js
@@ -0,0 +1,10 @@
+var nome = "Sidney"
+var idade = "∞";
+var estaSolteiro = true;
+
+if (estaSolteiro === true) {
+    console.log(nome + " na pista!!!")
+} else {
+    console.log(nome + " esta barrado do mundo de solteiro");
+}
+
